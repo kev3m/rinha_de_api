686e3b4641db284c4607d66f5f021c452e960ccb
Keven C <keven_jkc@hotmail.com>
2023-08-14 15:15:38 -0300

    chore: adição de recursos docker/postgre

--- src/main/java/com/api/rinha_de_backend/RinhaDeApiApplication.java
+++ src/main/java/com/api/rinha_de_backend/RinhaDeApiApplication.java
@@ -1,9 +1,14 @@
 package com.api.rinha_de_backend;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.api.rinha_de_backend.entities") //Scan all entities
+@EnableJpaRepositories("com.api.rinha_de_backend.controller") //Scan all repositories -> interfaces que fazem acesso ao banco de dados
 public class RinhaDeApiApplication {
 
     public static void main(String[] args) {
