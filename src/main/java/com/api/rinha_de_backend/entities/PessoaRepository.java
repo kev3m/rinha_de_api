5c4b8da96ea9731a4474c161b3c9fc939480c3e8
Keven C <keven_jkc@hotmail.com>
2023-08-15 23:04:57 -0300

    add: interface de repositório JPA para conexão com postgre

---
+++ src/main/java/com/api/rinha_de_backend/entities/PessoaRepository.java
@@ -0,0 +1,7 @@
+package com.api.rinha_de_backend.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+}
