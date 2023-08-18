369358d45e3dc925539cfba17957c56d99ed56ec
Keven C <keven_jkc@hotmail.com>
2023-08-17 21:32:25 -0300

    fix: Correção da nomenclatura dos pacotes repositórios JPA

--- src/main/java/com/api/rinha_de_backend/RinhaDeApiApplication.java
+++ src/main/java/com/api/rinha_de_backend/RinhaDeApiApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication // Enable Spring Boot auto config and component scanning
 @EntityScan("com.api.rinha_de_backend.entities") //Scan all entities
-@EnableJpaRepositories("com.api.rinha_de_backend.controller") //Scan all repositories -> interfaces que fazem acesso ao banco de dados
+@EnableJpaRepositories("com.api.rinha_de_backend") //Scan all repositories -> interfaces que fazem acesso ao banco de dados
 public class RinhaDeApiApplication {
 
     public static void main(String[] args) {
